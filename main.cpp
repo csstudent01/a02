ba2b2caeb4e5d28aa8b28b2cf9a4f432951dca78
VBKE15 <86685688+csstudent01@users.noreply.github.com>
2021-07-08 08:36:16 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+int season;
+int rateFall = 42.42;
+int rateWinter = 50.00;
+int rateSpring = 75.00;
+int rateSummer = 100;
+int numRooms;
+int daysStay;
+float salesTax;
+
+double totalCostDiscount;
+
+
+
+double fallPrice
+
+
+// user input
+//cin << salesTax;
+
+//total = reservation + tax +w/ without roomDiscount
+
+
+cout << "Welcome to Resort 161. Answer some questions and i can calculate the reservation cost for your booking." << endl;
+cout << endl;
+
+cout << "Pick an option from below";
+cout << "SEASON: [1]Fall [2]Winter [3]Spring [4]Summer: ";
+
+cin >> season;
+
+cout << "NUMBER OF ROOMS: ";
+cin >> numRooms;
+
+cout << "DAYS OF STAY: ";
+cin >> daysStay; 
+
+cout << "TAX RATE: ";
+cin >> salesTax;
+
+cout << endl;
+cout << endl;
+
+
+  if(rateFall) {
+    total = 42.42 * daysStay * numRooms * salesTax;
+
+    cout << "Season:      ";
+    cin >>
+    cout << "Price per night:     ";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
